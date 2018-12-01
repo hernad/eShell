516121de2e02d4f9a025939977ec543432c3b8f0
Ernad Husremovic <hernad@bring.out.ba>
2018-12-01 20:35:48 +0100

    delete debug ts

---
+++ scripts/delete_debug.sh
@@ -0,0 +1,8 @@
+rm -rf src/vs/workbench/api/node/extHostDebugService.ts
+rm -rf src/vs/workbench/parts/debug/browser/debugActions.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/callStackView.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/debug.contribution.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/debugHover.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/repl.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/variablesView.ts
+rm -rf src/vs/workbench/parts/debug/electron-browser/watchExpressionsView.ts
