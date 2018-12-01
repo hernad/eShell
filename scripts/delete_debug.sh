5d0449a3df7fd65c95110dded49258052d2bf4c2
Ernad Husremovic <hernad@bring.out.ba>
2018-12-01 21:20:23 +0100

    delete debug

--- scripts/delete_debug.sh
+++ scripts/delete_debug.sh
@@ -1,8 +1,10 @@
-rm -rf src/vs/workbench/api/node/extHostDebugService.ts
-rm -rf src/vs/workbench/parts/debug/browser/debugActions.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/callStackView.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/debug.contribution.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/debugHover.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/repl.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/variablesView.ts
-rm -rf src/vs/workbench/parts/debug/electron-browser/watchExpressionsView.ts
+#!/bin/bash
+
+rm  src/vs/workbench/api/node/extHostDebugService.ts
+rm  src/vs/workbench/parts/debug/browser/debugActions.ts
+rm  src/vs/workbench/parts/debug/electron-browser/callStackView.ts
+rm  src/vs/workbench/parts/debug/electron-browser/debug.contribution.ts
+rm  src/vs/workbench/parts/debug/electron-browser/debugHover.ts
+rm  src/vs/workbench/parts/debug/electron-browser/repl.ts
+rm  src/vs/workbench/parts/debug/electron-browser/variablesView.ts
+rm  src/vs/workbench/parts/debug/electron-browser/watchExpressionsView.ts
