20a2c5211c768d49597f3b2fbdad76c2c3d0f09f
Ernad Husremovic <hernad@bring.out.ba>
2019-01-08 13:05:16 +0100

    welcomePage out

--- src/vs/workbench/workbench.main.ts
+++ src/vs/workbench/workbench.main.ts
@@ -80,7 +80,7 @@ import 'vs/workbench/parts/extensions/electron-browser/extensions.contribution';
 import 'vs/workbench/parts/extensions/browser/extensionsQuickOpen';
 import 'vs/workbench/parts/extensions/electron-browser/extensionsViewlet';
 
-import 'vs/workbench/parts/welcome/page/electron-browser/welcomePage.contribution';
+// import 'vs/workbench/parts/welcome/page/electron-browser/welcomePage.contribution';
 
 import 'vs/workbench/parts/output/electron-browser/output.contribution';
 import 'vs/workbench/parts/output/browser/outputPanel';
