364123e45dfdb0d03a87823b5fa32b1f11fe8081
Ernad Husremovic <hernad@bring.out.ba>
2019-01-08 08:26:48 +0100

    hard-coded 120x40 (cols x rows)

--- src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
@@ -1062,6 +1062,9 @@ export class TerminalInstance implements ITerminalInstance {
 			rows = Math.min(Math.max(this._dimensionsOverride.rows, 2), this._rows);
 		}
 
+		cols = 120;
+		rows = 40;
+
 		if (this._xterm) {
 			const font = this._configHelper.getFont(this._xterm);
 
