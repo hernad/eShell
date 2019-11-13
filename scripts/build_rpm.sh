6cf247fed43e0508fb750fa887b64aaa557c96bd
hernad <hernad@bring.out.ba>
2019-11-13 09:23:48 +0100

    build rpm

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -8,3 +8,8 @@ yarn gulp minify-vscode
 yarn gulp vscode-linux-x64-min-ci
 
 yarn gulp vscode-linux-x64-build-rpm
+
+ls -lh .build/linux/rpm/x86_64/eShell-.*el7.x86_64.rpm
+
+
+echo 'sudo rpm -Uvh .build/linux/rpm/x86_64/eShell-*.el7.x86_64.rpm'
