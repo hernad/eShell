4e4c3b4033884fc135b1704d4cf3ba5b57a9991d
hernad <hernad@bring.out.ba>
2019-12-27 20:00:09 +0100

    BUILD_RPM, BUILD_DEB

--- upload_bintray_linux.sh
+++ upload_bintray_linux.sh
@@ -55,61 +55,68 @@ JSON_CONTENT="{\"name\":\"${BINTRAY_PACKAGE_VER}\", \"desc\": \"verzija ${BINTRA
 echo creating bintray RPM version ...
 echo JSON_CONTENT="$JSON_CONTENT"
 
-echo publish version ${BINTRAY_PACKAGE_VER}
-curl -X POST \
-      -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-    --header "Content-Type: application/json" \
-		--header "X-Bintray-Publish: 1" \
-    -d "$JSON_CONTENT" \
-     https://api.bintray.com/packages/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/versions
-
-
-echo uploading RPM $FILE to bintray ...
-curl -s -T $FILE \
-      -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-      --header "X-Bintray-Override: 1" \
+if [ "$BUILD_RPM" != "0" ] ; then
+	echo publish version ${BINTRAY_PACKAGE_VER}
+	curl -X POST \
+				-u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+			--header "Content-Type: application/json" \
 			--header "X-Bintray-Publish: 1" \
-     https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$FILE
-
-#curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-#   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
-
-echo  "recalc rpm metadata"
-curl -X POST -u ${BINTRAY_OWNER}:${BINTRAY_API_KEY} \
-   https://api.bintray.com/calc_metadata/$BINTRAY_OWNER/$BINTRAY_REPOS
+			-d "$JSON_CONTENT" \
+			https://api.bintray.com/packages/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/versions
 
 
-if [ "$VSCODE_ARCH" == "x64" ]; then
-  BINTRAY_REPOS=deb-x64
-  BINTRAY_PACKAGE=eshell
-  DEB_ARCH=amd64
+	echo uploading RPM $FILE to bintray ...
+	curl -s -T $FILE \
+				-u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+				--header "X-Bintray-Override: 1" \
+				--header "X-Bintray-Publish: 1" \
+			https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$FILE
+
+	#curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+	#   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
+
+	echo  "recalc rpm metadata"
+	curl -X POST -u ${BINTRAY_OWNER}:${BINTRAY_API_KEY} \
+		https://api.bintray.com/calc_metadata/$BINTRAY_OWNER/$BINTRAY_REPOS
 else
-  BINTRAY_REPOS=deb-x86
-  BINTRAY_PACKAGE=eshell
-  DEB_ARCH=i386
+	echo "no publish RPM"
 fi
-DEB_POOL=pool/main/e/eshell
-#DEB_INFO=deb_distribution=mydist;deb_component=main;deb_architecture=i386,amd64
-DEB_INFO="deb_distribution=bout;deb_component=main;deb_architecture=$DEB_ARCH"
 
-# https://blog.bintray.com/2014/12/16/hosting-debian-packages-on-bintray-rocks/
-# https://api.bintray.com/content/myorg/myrepo/mypack/0.0.1/pool/main/m/mypack/libmypack_0.0.1_i386.deb;deb_distribution=mydist;deb_component=main;deb_architecture=i386,amd64
+if [ "$BUILD_DEB" != "0" ] ; then
+	if [ "$VSCODE_ARCH" == "x64" ]; then
+		BINTRAY_REPOS=deb-x64
+		BINTRAY_PACKAGE=eshell
+		DEB_ARCH=amd64
+	else
+		BINTRAY_REPOS=deb-x86
+		BINTRAY_PACKAGE=eshell
+		DEB_ARCH=i386
+	fi
+	DEB_POOL=pool/main/e/eshell
+	#DEB_INFO=deb_distribution=mydist;deb_component=main;deb_architecture=i386,amd64
+	DEB_INFO="deb_distribution=bout;deb_component=main;deb_architecture=$DEB_ARCH"
 
-mv .build/linux/deb/${DEB_ARCH}/deb/*.deb .
-FILE=`ls *.deb`
+	# https://blog.bintray.com/2014/12/16/hosting-debian-packages-on-bintray-rocks/
+	# https://api.bintray.com/content/myorg/myrepo/mypack/0.0.1/pool/main/m/mypack/libmypack_0.0.1_i386.deb;deb_distribution=mydist;deb_component=main;deb_architecture=i386,amd64
 
-echo -e
-echo -e
-echo "DEB_PATH: $BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$DEB_POOL/$FILE;$DEB_INFO"
-echo "uploading DEB $FILE to bintray ..."
-curl -s -T $FILE \
-      -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-      --header "X-Bintray-Override: 1" \
-     "https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$DEB_POOL/$FILE;$DEB_INFO"
+	mv .build/linux/deb/${DEB_ARCH}/deb/*.deb .
+	FILE=`ls *.deb`
 
-curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
+	echo -e
+	echo -e
+	echo "DEB_PATH: $BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$DEB_POOL/$FILE;$DEB_INFO"
+	echo "uploading DEB $FILE to bintray ..."
+	curl -s -T $FILE \
+				-u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+				--header "X-Bintray-Override: 1" \
+			"https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$DEB_POOL/$FILE;$DEB_INFO"
 
-# recalc deb metadata
-curl -X POST -u ${BINTRAY_OWNER}:${BINTRAY_API_KEY} \
-   https://api.bintray.com/calc_metadata/$BINTRAY_OWNER/$BINTRAY_REPOS
+	curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+		-X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
+
+	# recalc deb metadata
+	curl -X POST -u ${BINTRAY_OWNER}:${BINTRAY_API_KEY} \
+		https://api.bintray.com/calc_metadata/$BINTRAY_OWNER/$BINTRAY_REPOS
+else
+	echo "no debian"
+fi
