3bfe477d6ec99ae5582176bfe2774aa6c8319968
hernad <hernad@bring.out.ba>
2019-12-27 18:10:02 +0100

    test windows azure pipelines

--- test_azure_pipelines.bat
+++ test_azure_pipelines.bat
@@ -5,18 +5,24 @@ set BINTRAY_REPOS=eShell
 set BINTRAY_PACKAGE=eShell-windows-%BINTRAY_ARCH%
 set CURL=curl.exe
 
+#BINTRAY_PACKAGE_VER=$BUILD_BUILDNUMBER
+
+
+REM #find .build
+REM #echo =======================================================
+REM #.build/win32-ia32/user-setup/eShellSetup.exe
 
 set NODE_PROG=const json=require('./package.json') ; console.log(json.version)
 echo %NODE_PROG% | node > tmpFile
 set /p BINTRAY_PACKAGE_VER= < tmpFile
 del tmpFile
 
-IF [%BINTRAY_ARCH%] EQU  [x64] move .build/win32-x64/user-setup/eShellSetup.exe eShellSetup-x64-%BINTRAY_PACKAGE_VER%.exe
-IF [%BINTRAY_ARCH%] EQU  [x64] set FILE=eShellSetup-x64-%BINTRAY_PACKAGE_VER%.exe
+IF [%BINTRAY_ARCH%] EQU  [x64] copy package.json package-%BINTRAY_PACKAGE_VER%.txt
+IF [%BINTRAY_ARCH%] EQU  [x64] set FILE=package-%BINTRAY_PACKAGE_VER%.txt
 
 
-IF [%BINTRAY_ARCH%] NEQ  [x64] move .build/win32-ia32/user-setup/eShellSetup.exe eShellSetup-x86-%BINTRAY_PACKAGE_VER%.exe
-IF [%BINTRAY_ARCH%] NEQ  [x64] set FILE=eShellSetup-x86-%BINTRAY_PACKAGE_VER%.exe
+IF [%BINTRAY_ARCH%] NEQ  [x64] move package.json package-%BINTRAY_PACKAGE_VER%.txt
+IF [%BINTRAY_ARCH%] NEQ  [x64] set FILE=package-%BINTRAY_PACKAGE_VER%.txt
 
 
 echo "======================== package: %BINTRAY_PACKAGE% ========== package_ver: %BINTRAY_PACKAGE_VER% =================="
