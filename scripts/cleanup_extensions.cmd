d7cb539d62ba3fc38b926a678b6f7ba8f59a5af8
Ernad Husremovic <hernad@bring.out.ba>
2020-07-13 18:50:07 +0200

    extension recommendations - out

---
+++ scripts/cleanup_extensions.cmd
@@ -0,0 +1,95 @@
+set RM=c:\cygwin64\bin\rm.exe
+
+%RM% -rf extensions\css-language-features
+%RM% -rf extensions\html-language-features
+%RM% -rf extensions\typescript-language-features
+%RM% -rf extensions\vscode-api-tests
+%RM% -rf extensions\markdown-language-features
+%RM% -rf extensions\vscode-web-playground
+%RM% -rf extensions\github-browser
+%RM% -rf extensions\github
+%RM% -rf extensions\merge-conflict
+%RM% -rf extensions\emmet
+%RM% -rf extensions\search-result
+%RM% -rf extensions\configuration-editing
+
+
+
+
+%RM% -rf extensions\docker
+%RM% -rf extensions\git
+%RM% -rf extensions\python
+
+
+
+%RM% -rf .github
+
+%RM% -rf src/vs/workbench/api/browser/mainThreadDebugService.ts
+%RM% -rf src/vs/workbench/api/browser/mainThreadSCM.ts
+%RM% -rf src/vs/workbench/api/browser/mainThreadDiagnostics.ts
+%RM% -rf src/vs/workbench/api/browser/mainThreadComments.ts
+%RM% -rf src/vs/workbench/api/browser/mainThreadLanguageFeatures.ts
+
+%RM% -rf src/vs/workbench/contrib/extensions/browser/extensionRecommendationsService.ts
+
+%RM% -rf src/vs/workbench/contrib/comments
+%RM% -rf src/vs/workbench/contrib/debug
+%RM% -rf src/vs/workbench/contrib/scm
+
+%RM% -rf src/vs/workbench/api/common/extHostLanguageFeatures.ts
+%RM% -rf src/vs/workbench/api/browser/mainThreadLanguages.ts
+
+
+%RM% -rf src/vs/workbench/api/node/extHostDebugService.ts
+
+%RM% -rf src/vs/workbench/api/common/extHostSCM.ts
+%RM% -rf src/vs/workbench/api/common/extHostDebugService.ts
+
+%RM% -rf src/vs/editor/test
+%RM% -rf src/vs/workbench/services/editor/test
+%RM% -rf src/vs/workbench/test
+%RM% -rf src/vs/platform/files/test
+%RM% -rf src/vs/editor/contrib/linesOperations/test
+%RM% -rf src/vs/editor/contrib/find/test
+%RM% -rf src/vs/editor/contrib/bracketMatching/test
+%RM% -rf src/vs/editor/contrib/multicursor/test
+%RM% -rf src/vs/editor/contrib/suggest/test
+%RM% -rf src/vs/editor/contrib/wordOperations/test
+%RM% -rf src/vs/editor/contrib/snippet/test
+%RM% -rf src/vs/editor/contrib/rename/test
+%RM% -rf src/vs/workbench/contrib/notebook/test
+%RM% -rf src/vs/workbench/contrib/extensions/test
+%RM% -rf src/vs/workbench/services/extensionManagement/test
+%RM% -rf src/vs/workbench/services/keybinding/test
+%RM% -rf src/vs/workbench/services/preferences/test
+%RM% -rf src/vs/workbench/services/uriIdentity/test
+%RM% -rf src/vs/workbench/services/workingCopy/test
+%RM% -rf src/vs/workbench/contrib/terminal/test
+
+%RM% -rf src/vs/workbench/contrib/*/test
+%RM% -rf src/vs/platform/*/test
+%RM% -rf src/vs/workbench/services/configuration/test/
+%RM% -rf src/vs/workbench/services/*/test/
+%RM% -rf src/vs/workbench/contrib/notebook/browser/contrib/fold/test
+%RM% -rf src/vs/workbench/contrib/notebook/browser/contrib/*/test
+
+
+
+
+%RM% -rf src/vs/workbench/services/textfile/test
+%RM% -rf src/vs/platform/userDataSync/test
+%RM% -rf src/vs/workbench/contrib/backup/test
+
+
+%RM% -rf src/vs/workbench/api/common/extHostDiagnostics.ts
+%RM% -rf src/vs/workbench/api/common/extHostLanguages.ts
+
+
+
+%RM% -rf src/vs/workbench/contrib/telemetry/browser/telemetry.contribution.ts
+%RM% -rf src/vs/workbench/services/telemetry/browser/telemetryService.ts
+%RM% -rf src/vs/workbench/services/telemetry/electron-browser/telemetryService.ts
+%RM% -rf src/vs/platform/telemetry/common/telemetryService.ts
+%RM% -rf src/vs/platform/telemetry/common/telemetryUtils.ts
+
+%RM% -rf src/vs/workbench/contrib/welcome/telemetryOptOut
