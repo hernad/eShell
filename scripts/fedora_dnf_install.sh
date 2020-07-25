ae74ad61c62c35821aec5c532a470de27c25bfb8
Ernad Husremovic <hernad@bring.out.ba>
2020-07-25 16:58:30 +0200

    install yarn

--- scripts/fedora_dnf_install.sh
+++ scripts/fedora_dnf_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo dnf -y groupinstall "Development Tools"
+
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo dnf -y install yarn
